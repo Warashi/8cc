e56d92e783c0e9c4a7751618dd5b201aa7ae241d
Shinnosuke Sawada <6warashi9@gmail.com>
2020-05-02 10:11:12 +0900

    トークナイザを実装

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,89 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token *next;
+  long val;
+  char *loc;
+  long len;
+};
+
+static void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+static bool equal(Token *tok, char *s) {
+  return strlen(s) == tok->len && !strncmp(tok->loc, s, tok->len);
+}
+
+static Token *skip(Token *tok, char *s) {
+  if (!equal(tok, s)) {
+    error("expected '%s'", s);
+  }
+  return tok->next;
+}
+
+static long get_number(Token *tok) {
+  if (tok->kind != TK_NUM) {
+    error("expected a number");
+  }
+  return tok->val;
+}
+
+static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->loc = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+static Token *tokenize(char *p) {
+  Token head = {};
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,28 +91,25 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  Token *tok = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  printf("  mov rax, %ld\n", get_number(tok));
+  tok = tok->next;
+
+  while (tok->kind != TK_EOF) {
+    if (equal(tok, "+")) {
+      printf("  add rax, %ld\n", get_number(tok->next));
+      tok = tok->next->next;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
+    tok = skip(tok, "-");
+    printf("  sub rax, %ld\n", get_number(tok));
+    tok = tok->next;
   }
 
   printf("  ret\n");
