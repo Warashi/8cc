863a7dc2ddca807eed673354b1cf9b00bd30869b
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-08 10:02:43 +0900

    最初からやりなおすぜ

--- 9cc.c
+++ 9cc.c
@@ -1,114 +1,5 @@
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-typedef enum {
-  TK_RESERVED,
-  TK_NUM,
-  TK_EOF,
-} TokenKind;
-
-typedef struct Token Token;
-struct Token {
-  TokenKind kind;
-  Token *next;
-  long val;
-  char *loc;
-  long len;
-};
-
-static char *current_input;
-
-static void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-static void verror_at(char *loc, char *fmt, va_list ap) {
-  int pos = loc - current_input;
-  fprintf(stderr, "%s\n", current_input);
-  fprintf(stderr, "%*s", pos, "");
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-static void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  verror_at(loc, fmt, ap);
-}
-
-static void error_tok(Token *tok, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  verror_at(tok->loc, fmt, ap);
-}
-
-static bool equal(Token *tok, char *s) {
-  return strlen(s) == tok->len && !strncmp(tok->loc, s, tok->len);
-}
-
-static Token *skip(Token *tok, char *s) {
-  if (!equal(tok, s)) {
-    error_tok(tok, "expected '%s'", s);
-  }
-  return tok->next;
-}
-
-static long get_number(Token *tok) {
-  if (tok->kind != TK_NUM) {
-    error_tok(tok, "expected a number");
-  }
-  return tok->val;
-}
-
-static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-  Token *tok = calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->loc = str;
-  tok->len = len;
-  cur->next = tok;
-  return tok;
-}
-
-static Token *tokenize() {
-  char *p = current_input;
-  Token head = {};
-  Token *cur = &head;
-
-  while (*p) {
-    if (isspace(*p)) {
-      p++;
-      continue;
-    }
-
-    if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p, 0);
-      char *q = p;
-      cur->val = strtol(p, &p, 10);
-      cur->len = p - q;
-      continue;
-    }
-
-    if (*p == '+' || *p == '-') {
-      cur = new_token(TK_RESERVED, cur, p++, 1);
-      continue;
-    }
-
-    error_at(p, "invalid token");
-  }
-
-  new_token(TK_EOF, cur, p, 0);
-  return head.next;
-}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -116,28 +7,10 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  current_input = argv[1];
-  Token *tok = tokenize();
-
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-
-  printf("  mov rax, %ld\n", get_number(tok));
-  tok = tok->next;
-
-  while (tok->kind != TK_EOF) {
-    if (equal(tok, "+")) {
-      printf("  add rax, %ld\n", get_number(tok->next));
-      tok = tok->next->next;
-      continue;
-    }
-
-    tok = skip(tok, "-");
-    printf("  sub rax, %ld\n", get_number(tok));
-    tok = tok->next;
-  }
-
+  printf("  mov rax, %d\n", atoi(argv[1]));
   printf("  ret\n");
   return 0;
 }
